5c03340c2076d0bcfac3f21ce835c9848946a7ad
Arya Devi <aryaarya96016@gmail.com>
2024-01-07 13:04:03 +0530

    admin product management - Image uploading(Image drag and drop using react-sortablejs,react spinner,multiparty is used to parsing form data,aws to upload file,mime-types used wheather the pic type(jpg,png) )

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,22 +1,27 @@
 import axios from "axios";
 import { useRouter } from "next/router";
 import { useState } from "react";
+import Spinner from "@/components/Spinner";
+import { ReactSortable } from "react-sortablejs";
 
 export default function ProductForm({
   _id,
   title: existingTitle,
   description: existingDescription,
   price: existingPrice,
+  images: existingImage,
 }) {
   const [title, setTitle] = useState(existingTitle || "");
   const [description, setDescription] = useState(existingDescription || "");
   const [price, setPrice] = useState(existingPrice || "");
   const [goToProduct, setGoToProduct] = useState(false);
+  const [isUploading, setIsUploading] = useState(false);
+  const [images, setImages] = useState(existingImage || []);
   const router = useRouter();
 
   async function saveProduct(ev) {
     ev.preventDefault();
-    const data = { title, description, price };
+    const data = { title, description, price, images };
     //update
     if (_id) {
       await axios.put("/api/products", { ...data, _id });
@@ -29,7 +34,24 @@ export default function ProductForm({
   if (goToProduct) {
     router.push("/products");
   }
-
+  async function uploadImages(ev) {
+    const files = ev.target?.files;
+    if (files?.length > 0) {
+      setIsUploading(true);
+      const data = new FormData();
+      for (const file of files) {
+        data.append("file", file);
+      }
+      const res = await axios.post("/api/upload", data);
+      setImages((oldImages) => {
+        return [...oldImages, ...res.data.links];
+      });
+      setIsUploading(false);
+    }
+  }
+  function updateImagesOrder(images) {
+    setImages(images);
+  }
   return (
     <form onSubmit={saveProduct}>
       <label>Product Name</label>
@@ -39,6 +61,43 @@ export default function ProductForm({
         value={title}
         onChange={(ev) => setTitle(ev.target.value)}
       />
+      <label>Photos</label>
+      <div className="mb-2 flex flex-wrap gap-1">
+        <ReactSortable className="flex flex-wrap gap-1"
+        list={images} 
+        setList={updateImagesOrder}>
+          {!!images?.length &&
+            images.map((link) => (
+              <div key={link} className="h-24">
+                <img src={link} alt="" className="rounded-lg" />
+              </div>
+            ))}
+        </ReactSortable>
+
+        {isUploading && (
+          <div className="h-24 flex items-center">
+            <Spinner />
+          </div>
+        )}
+        <label className="w-24 h-24 cursor-pointer text-center flex flex-col items-center justify-center text-sm text-gray-800 rounded-lg bg-slate-400">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          <div>Upload</div>
+          <input type="file" onChange={uploadImages} className="hidden" />
+        </label>
+      </div>
       <label>Description</label>
       <textarea
         placeholder="Description"
---
+++ components/Spinner.js
@@ -0,0 +1,4 @@
+import { ClimbingBoxLoader } from "react-spinners";
+export default function Spinner() {
+  return <ClimbingBoxLoader color={"#1E3A8A"} speedMultiplier={2} />;
+}
