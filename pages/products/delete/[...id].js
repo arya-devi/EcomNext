4e89f5ba6a1138d9b03f3ac83e51b385383458b8
arya-devi <147596834+arya-devi@users.noreply.github.com>
2024-02-07 13:32:16 +0530

    ESlint issue solved

--- pages/products/delete/[...id].js
+++ pages/products/delete/[...id].js
@@ -25,7 +25,7 @@ export default function DeleteProductPage() {
   return (
     <Layout>
       <h1 className="text-center">
-        Do you really want to delete this product&nbsp;"{productInfo?.title}" ?
+        Do you really want to delete this product&nbsp;{productInfo?.title} ?
       </h1>
       <div className="flex gap-2 justify-center">
         <button className="btn-red" onClick={deleteProduct}>
